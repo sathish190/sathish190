f6bcfea23279f00051adf7f06afa2150e45682df
sathish190 <152969852+sathish190@users.noreply.github.com>
2023-12-13 15:26:54 +0530

    Add files via upload

---
+++ GreatestThree.java
@@ -0,0 +1,22 @@
+//Program to find the Greatest among three numbers.
+
+import java.util.Scanner;
+
+public class GreatestThree
+{
+  public static void main(String args[])
+  {
+   Scanner reader = new Scanner(System.in);
+   System.out.print("\nEnter the number ----");
+   System.out.print("\nEnter the first number = ");
+   int fn = reader.nextInt();
+   System.out.print("\nEnter the second number = ");
+   int sn = reader.nextInt();
+   System.out.print("\nEnter the thrid number = ");
+   int tn = reader.nextInt();
+   
+
+   reader.close();
+  }
+}
+   
